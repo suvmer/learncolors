6a2180d86757702886e91ac889a829317e5a913f
duhan <duhan@LAPTOP-1BP174QR>
2023-03-21 21:02:51 +0300

    removed useEffect to reset Answer button

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -2,25 +2,26 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Context } from '../App';
 
 const ColorPicker = () => {
-  const {steps, tasks, onClickVariant, goBack} = useContext(Context);
+  const {steps, tasks, onClickVariant, goBack/*, answers*/} = useContext(Context);
   const task = tasks[steps[0]];
   const [tip, setTip] = useState("Ответ");
   const [back, setBack] = useState(false);
-  useEffect(() => setTip("Ответ"), [steps[0]]); //reset button after answer
-  console.log("this is", task);
+  console.log("this is {`button small ${answers[0][steps[0]] == ind ? 'high' : ''}`}", task);
   return (
     <>
         <div className="colorBox" style={{backgroundColor: task.list[task.correct][0]}}>
       
         </div>
         <div className='list'>
-        {task.list.map((element, ind) => <div key={ind} style={{backgroundColor: back ? element[0] : "#61dafb00"}} onClick={() => onClickVariant(ind)} className='button small'>{element[1]}</div>)}
+        {task.list.map((element, ind) => <div key={ind} style={{backgroundColor: back ? element[0] : "#61dafb00"}} onClick={
+          () => onClickVariant(ind)} className='button small'>{element[1]}</div>)
+          }
         </div>
         <div className='list'>
-        <div onClick={goBack} className='button small'>Назад</div>
-        <div onClick={() => setTip(task.list[task.correct][1])} className='button small'>{tip}</div>
+        <div onClick={() => {goBack(); setTip("Ответ"); setBack(false); }} className='button small'>Назад</div>
+        <div onClick={() => {setTip(task.list[task.correct][1]); setBack(false)}} className='button small'>{tip}</div>
         <div onClick={() => setBack(!back)} className='button small'>Подсветить</div>
-        <div onClick={() => onClickVariant(-1)} className='button small'>Вперёд</div>
+        <div onClick={() => {setTip("Ответ"); onClickVariant(-1); setBack(false)}} className='button small'>Вперёд</div>
         </div>
     </>
   );
