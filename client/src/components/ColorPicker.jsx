d63303c956bbfbcce4d0d42c4bddf4314ba8faa5
suvmer <suvmer@mail.ru>
2023-04-12 23:04:58 +0300

    -comments2

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -7,19 +7,16 @@ const ColorPicker = () => {
   const game = useSelector(state => state.game);
   const dispatch = useDispatch();
   
-  const goChange = (st) => {
-    dispatch({
-      type: "CHANGEPAGE",
-      payload: st
-    })
-  }
+  
 
   const task = game.questions[game.step];
   const [tip, setTip] = useState("Ответ");
   const [back, setBack] = useState(false);
 
   const selectAnswer = (index) => {
-     if(game.answers[game.step] == -1) {
+    setTip("Ответ");
+    setBack(false);
+    if(game.answers[game.step] == -1) {
       if(index == game.questions[game.step].correct) {
         dispatch({
           type: "CHANGESCORE",
@@ -33,7 +30,15 @@ const ColorPicker = () => {
     }
   }
 
-  console.log(game);
+  const goChange = (st) => {
+    setTip("Ответ");
+    setBack(false);
+    dispatch({
+      type: "CHANGEPAGE",
+      payload: st
+    })
+  }
+
   return (
     <>
         <div className="colorBox" style={{backgroundColor: task.list[task.correct][0]}}>
@@ -45,10 +50,10 @@ const ColorPicker = () => {
           }
         </div>
         <div className='list'>
-        <div onClick={() => {goChange(-1); setTip("Ответ"); setBack(false); }} className='button small'>Назад</div>
-        <div onClick={() => {setTip(task.list[task.correct][1]); setBack(false)}} className='button small'>{tip}</div>
-        <div onClick={() => setBack(!back)} className='button small'>Подсветить</div>
-        <div onClick={() => {setTip("Ответ"); goChange(1); setBack(false)}} className='button small'>Вперёд</div>
+          <div onClick={() => goChange(-1)} className='button small'>Назад</div>
+          <div onClick={() => {setTip(task.list[task.correct][1]); setBack(false)}} className='button small'>{tip}</div>
+          <div onClick={() => setBack(!back)} className='button small'>Подсветить</div>
+          <div onClick={() => goChange(1)} className='button small'>Вперёд</div>
         </div>
     </>
   );
