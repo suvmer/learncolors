3ec64aa86d584511ddfc800f65d0f01f897af069
suvmer <suvmer@mail.ru>
2023-07-05 20:06:44 +0300

    count forms added

--- client/src/pages/Staticstics.jsx
+++ client/src/pages/Staticstics.jsx
@@ -1,3 +1,5 @@
+import { CountForm } from "../utils/utils";
+
 export const Statistics = ({game, resetGame}) => {
     const countCorrect = game.answers.reduce((acc, el, ind) => acc + (el == game.questions[ind].correct), 0);
     return <>
@@ -12,7 +14,7 @@ export const Statistics = ({game, resetGame}) => {
                     </div>
                 </div>;
             })}
-            <p>Вы набрали <mark className="yellow">{game.score}</mark> очков, ответив верно на <mark className="yellow">{countCorrect}</mark> вопроса!</p>
+            <p>Вы набрали <mark className="yellow">{game.score}</mark> {CountForm(game.score, ["очко", "очка", "очков"])}, ответив верно на <mark className="yellow">{countCorrect}</mark> {CountForm(countCorrect, ["вопрос", "вопроса", "вопросов"])}!</p>
             <div onClick={() => resetGame(game)} className="button">Пройти заново</div>
             <div onClick={() => resetGame()} className="button">Новая игра</div>
         </div>
--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -2,8 +2,16 @@ import {namedColors} from '../resources/colors';
 
 const random = (arr) => arr[Math.floor((Math.random()*arr.length))];
 
-const generateTasks = (tasksCount, answersCount) => Array(tasksCount).fill().map(el => ({
+export const generateTasks = (tasksCount, answersCount) => Array(tasksCount).fill().map(el => ({
         list: Array(answersCount).fill().map(ans => random(namedColors)),
         correct: Math.floor((Math.random()*answersCount))
-    }))
-export {generateTasks};
+    }));
+
+export const CountForm = (number, titles) => {
+  number = Math.abs(number);
+  if (Number.isInteger(number)) {
+    const cases = [2, 0, 1, 1, 1, 2];  
+    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
+  }
+  return titles[1];
+}
