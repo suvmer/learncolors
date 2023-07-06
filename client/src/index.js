8f9a060ba54921d3904b01012bafc18153ac92f3
suvmer <suvmer@mail.ru>
2023-07-06 13:26:25 +0300

    no redux now

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './store/index';
-import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-  <Provider store={store}>
-      <App />
-  </Provider>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
